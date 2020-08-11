c82195fb51d5fadba39db776c5e5c191ea711744
Mugheera Bin Saidq <mugheerasadiq@gmail.com>
2020-08-11 19:10:06 +0500

    React Router

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,7 +4,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 
     function RenderMenuItem({dish, onClick}){
         return(
-            <Card onClick={() => onClick(dish.id)}>
+            <Card>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
@@ -18,7 +18,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
         const menu = props.dishes.map((dish) => {
             return (
               <div  className="col-12 col-md-5 m-1" key={dish.id}>
-                <RenderMenuItem dish={dish} onClick={props.onClick} />
+                <RenderMenuItem dish={dish} />
               </div>
                 );
         });
